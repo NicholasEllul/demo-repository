9fc16e8fa0054b1585bf3770f7c754c39256d32e
Nicholas Ellul <nicholas.ellul1@gmail.com>
2024-03-20 14:56:10 -0400

    Update marquee.js

--- marquee.js
+++ marquee.js
@@ -1,15 +1,1 @@
-let ran = false
 
-run();
-
-
-function run(){
-  alert("Compromised");
-  const marquee = document.createElement('marquee');
-  
-  marquee.innerText = 'This is a test';
-  marquee.style.width = '100%';
-  marquee.style.fontSize = '50px';
-  document.body.appendChild(marquee);
-  ran=true;
-}
