43ff0ce0d104462cdaa5589c927b0ad4ca44c0da
Nicholas Ellul <nicholas.ellul1@gmail.com>
2024-03-20 14:33:54 -0400

    Create marquee.js

---
+++ marquee.js
@@ -0,0 +1,7 @@
+const marquee = document.createElement('marquee');
+
+marquee.innerText = 'This is a test';
+marquee.style.width = '100%';
+marquee.style.fontSize = '24px';
+
+document.body.appendChild(marquee);
